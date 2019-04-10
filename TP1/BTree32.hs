297664bcf18fd1a33e074c33296179b92a4bc108
gpaoloni <paolonigianfranco@gmail.com>
2019-04-09 22:58:44 -0300

    Type Error and sintax fixes

--- TP1/BTree32.hs
+++ TP1/BTree32.hs
@@ -1,3 +1,5 @@
+module BTree32 where
+
 data BTree32 k a = Nil               --  ́arbol vacío
                  | Node
                    (BTree32 k a)     -- sub ́arbol izquierdo
@@ -9,11 +11,11 @@ size :: BTree32 k a -> Int
 size Nil            = 0
 size (Node _ n _ _) = n
 
-search :: Ord k => k -> BTree k a -> Maybe a
+search :: Ord k => k -> BTree32 k a -> Maybe a
 search _ Nil                 = Nothing
 search k (Node l _ (c,x) r) | k == c = Just x
                              | k <  c = search k l
-                             | Other  = search k r
+                             | otherwise  = search k r
 
 -- Contruye un BTree32 Node con la estructura solicitada
 node :: BTree32 k a -> (k,a) -> BTree32 k a -> BTree32 k a
@@ -21,13 +23,13 @@ node l x r = Node l sz x r
             where sz = size l + 1 + size r
 
 singleR :: Ord k => BTree32 k a -> (k,a) -> BTree32 k a -> BTree32 k a
-singleR (Node (Node lb _ b rb) _ a ra) = node lb b (node rb a ra)
+singleR (Node lb _ b rb) a ra = node lb b (node rb a ra)
 
 doubleR :: Ord k => BTree32 k a -> (k,a) -> BTree32 k a -> BTree32 k a
-doubleR (Node (Node lb _ b (Node lc _ c rc)) _ a ra) = node (node lb b lc) c (node rc a ra)
+doubleR (Node lb _ b (Node lc _ c rc)) a ra = node (node lb b lc) c (node rc a ra)
 
 singleL :: Ord k => BTree32 k a -> (k,a) -> BTree32 k a -> BTree32 k a
-singleL (Node la _ a (Node lb _ b rb)) = node (node la a lb) b rb
+singleL la a (Node lb _ b rb) = node (node la a lb) b rb
 
 doubleL :: Ord k => BTree32 k a -> (k,a) -> BTree32 k a -> BTree32 k a
-doubleL (Node la _ a (Node (Node lc _ c rc) _ b rb) = node (node la a lc) c (node rc b rb) 
+doubleL la a (Node (Node lc _ c rc) _ b rb) = node (node la a lc) c (node rc b rb)
