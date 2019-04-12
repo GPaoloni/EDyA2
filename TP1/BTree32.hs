721132813870cf8da202f9475ebbcaf6e574e468
gpaoloni <paolonigianfranco@gmail.com>
2019-04-12 18:32:12 -0300

    Implemented balance and insert

--- TP1/BTree32.hs
+++ TP1/BTree32.hs
@@ -6,6 +6,7 @@ data BTree32 k a = Nil               --  ́arbol vacío
                    Int               -- tamaño del árbol
                    (k,a)             -- elemento del nodo
                    (BTree32 k a)     -- sub ́arbol derecho
+                   deriving Show
 
 size :: BTree32 k a -> Int
 size Nil            = 0
@@ -33,3 +34,25 @@ singleL la a (Node lb _ b rb) = node (node la a lb) b rb
 
 doubleL :: Ord k => BTree32 k a -> (k,a) -> BTree32 k a -> BTree32 k a
 doubleL la a (Node (Node lc _ c rc) _ b rb) = node (node la a lc) c (node rc b rb)
+
+balance :: Ord k => BTree32 k a -> (k , a) -> BTree32 k a -> BTree32 k a
+balance l (k,v) r | size r + size l <= 1 = node l (k,v) r
+                  | size r > 3 * size l  = reBalanceL l (k,v) r
+                  | size l > 3 * size r  = reBalanceR l (k,v) r
+                  | otherwise            = node l (k,v) r
+                      
+reBalanceL :: Ord k => BTree32 k a -> (k , a) -> BTree32 k a -> BTree32 k a
+reBalanceL l (k,v) r | isSingle r = singleL l (k,v) r
+                     | otherwise  = doubleL l (k,v) r
+                     where isSingle (Node ll _ x rr) = size rr > 2 * size ll
+                      
+reBalanceR :: Ord k => BTree32 k a -> (k , a) -> BTree32 k a -> BTree32 k a
+reBalanceR l (k,v) r | isSingle l = singleR l (k,v) r
+                     | otherwise  = doubleR l (k,v) r
+                     where isSingle (Node ll _ x rr) = size ll > 2 * size rr
+
+insert :: Ord k => (k , a) -> BTree32 k a -> BTree32 k a
+insert (j,x) Nil                = node Nil (j,x) Nil
+insert (j,x) (Node l _ (k,v) r) | j > k      = balance l (k,v) (insert (j,x) r)
+                                | j < k      = balance (insert (j,x) l) (k,v) r
+                                | otherwise  = node l (k,x) r
