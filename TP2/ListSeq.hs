b8b912e34cace9ad211a496359c0957a0ba686da
Alumno <angie.ivaldi@gmail.com>
2019-05-31 18:18:05 -0300

    Advances in scan (not working

--- TP2/ListSeq.hs
+++ TP2/ListSeq.hs
@@ -4,51 +4,46 @@ import Seq
 import Par
 
 
-
 instance Seq [] where
-	emptyS         			= []
-	singletonS x   			= [x]
-	lengthS 	 			= length
-	nthS		    	    = (!!)
-	tabulateS f 0 		    = []
-	tabulateS f n  			= let (h,t) = f 0 ||| tabulateS (f . (+1)) (n-1) 
-							in h:t
-	mapS f []				= []
-	mapS f (x:xs)			= let (h,t) = f x ||| map f xs 
-							in h:t
-	filterS p []			= []
-	filterS p (x:xs)	 	= let (truthy,t) = p x ||| filterS p xs
-							in if truthy then x:t 
-							   else t
-	appendS 				= (++)
-	takeS 					= take
-	dropS 					= drop
-	showtS []				= EMPTY
-	showtS [x]				= ELT x
-	showtS xs				= let (l,r) = take n xs ||| drop n xs
-							  in NODE l r
-					where n = lengthS xs `div` 2
-	showlS []				= NIL 
-	showlS (x:xs)			= CONS x $ showlS xs
-	joinS					= concat TODO: CONSULTAR CON CECI
-	reduceS f e []			= e
-	reduceS f e [x]			= x
-	reduceS f e (x:y:xs)	= let (h,t) = f x y ||| reduceS f e xs
-							  in f h t
-	scanS f e []			= ([], e)
-	scanS f e [x]	 		= ([e], f e x)
-	scanS f e  s	        = let s' = scanS f e $ contract s
-							  in expand s s'
-					where contract []       = []
-						  contract [x]      = [x]
-						  contract (x:y:xs) = let (h,t) = f x y ||| contract xs
-						                      in h:t
-						  expand xs (ys, r)	= (mergeAndReduce xs ys, r)
-						  mergeAndReduce 
-						   a -> (s a, a)
-	fromList   :: [a] -> s a
-
-	
-	
-	
-	
+  emptyS         			= []
+  singletonS x   			= [x]
+  lengthS 	 			= length
+  nthS		    	    = (!!)
+  tabulateS f 0 		    = []
+  tabulateS f n  			= let (h,t) = f 0 ||| tabulateS (f . (+1)) (n-1)
+                        in h:t
+  mapS f []				= []
+  mapS f (x:xs)			= let (h,t) = f x ||| map f xs
+                      in h:t
+  filterS p []			= []
+  filterS p (x:xs)	 	= let (truthy,t) = p x ||| filterS p xs
+                        in if truthy then x:t else t
+  appendS 				= (++)
+  takeS n xs			= take xs n
+  dropS n xs			= drop xs n
+  showtS []				= EMPTY
+  showtS [x]				= ELT x
+  showtS xs				= let (l,r) = take n xs ||| drop n xs
+                    in NODE l r
+               where n = lengthS xs `div` 2
+  showlS []				= NIL
+  showlS (x:xs)			= CONS x $ showlS xs
+  joinS					= concat
+  reduceS f e []			= e
+  reduceS f e [x]			= x
+  reduceS f e (x:y:xs)	= let (h,t) = f x y ||| reduceS f e xs
+                					in f h t
+  scanS f e []			= ([], e)
+  scanS f e [x]	 		= ([e], f e x)
+  scanS f e s 	    = let s' = scanS f e $ contract s
+  							  		in expand s s'
+            where
+            contract []   = []
+            contract [x]        = [x]
+            contract (x:y:xs)   = let (h,t) = f x y ||| contract xs
+                                  in h:t
+            expand xs (ys, r)	  = (completeScan xs ys, r)
+            completeScan [x] ys = ys
+            completeScan xs ys  = tabulateS (\i -> buildIthItem i xs ys) (lengthS xs)
+            buildIthItem i xs ys | even i    = nthS ys $ i `div` 2
+                                 | otherwise = f (nthS ys $ i `div` 2) (nthS xs $ i-1)
